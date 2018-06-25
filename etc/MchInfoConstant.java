664aecda54d07610e835203fbafec9a7bc043271
xuanyunze <13552005525@139.com>
2018-06-25 15:37:46 +0800

    second

---
+++ etc/MchInfoConstant.java
@@ -0,0 +1,16 @@
+package com.payvx.payvxproxy.constant;
+
+/**
+ * 商户注册常量
+ * @author Goo Tsung-jen
+ * @create 2017-12-14  16:27
+ * Innovation distinguishes between a leader and a follower.
+ */
+public interface MchInfoConstant {
+    /** 商户开通状态，0-未开通，1-开通，2-停用，3-注销.*/
+    Integer status=1;
+
+    /** 商户类型.*/
+    Integer type=1;
+
+}
